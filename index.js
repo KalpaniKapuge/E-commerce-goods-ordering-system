22429504c5fc941cfd664459399482e861feafb8
KalpaniKapuge <kalpanikapuge1020@gmail.com>
2025-07-01 11:53:38 +0530

    Connected to the mongodb database🙌

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ import express from "express";
 
 const app =  express();
 
+mongoose.connect("mongodb+srv://KalpaniKapuge:KDKapuge5670@cluster0.igyzv3u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(
+    () => {
+        console.log("Connected to the database");
+    }
+).catch(
+    () => {
+        console.log("Database Connection failed");
+    }
+)
 
 app.listen(3000,() => {
     console.log("Server is running on prot 3000")
